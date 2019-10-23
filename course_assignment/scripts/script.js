888c3b3a2ba7eac5a2820f660e6d89e54c878e04
siljeheldalmonsen <silje24@yahoo.com>
2019-10-23 14:18:24 +0200

    got all my cards to work

--- course_assignment/scripts/script.js
+++ course_assignment/scripts/script.js
@@ -2,31 +2,34 @@
 // connect to this api https://api.magicthegathering.io/v1/cards
 
 function makeCards(json) {
-    var cardArray = json.cards;
 
-    let output = "";
-// get your cards array from the json object and then loop through them.
-cardArray.forEach(function(card) {
-console.log(card.name)
-output += ` <div class = "col-sm-4">
-            <div class = "card-container">
-            <h4> Card Name: ${card.name}> <h4>
-            <img scr = "${card.imageURL}">
-            <a href = "card-specific.html?id=${card.id}" class="btn btn-success">View More</a>
-            </div>
-            </div> `;
-})
-document.getElementById('output').innerHTML = output;
+	console.log(json)
+	var cardArray = json.cards;
+	let output = "";
+
+	// get your cards array from the json object and then loop through them.
+	cardArray.forEach(function(card) {
+
+		console.log(card.name)
+		output += `<div class="col-sm-4">
+					<div class="card-container">
+						<h4>${card.name}><h4>
+						<img scr="${card.imageURL}">
+						<a href="card-specific.html?id=${card.id}" class="btn btn-success">View More</a>
+					</div>
+				</div>`;
+	})
+	document.getElementById('cards').innerHTML = output;
 }
 
 
 fetch('https://api.magicthegathering.io/v1/cards')
-// the first .then convert the result to JSON format.
-.then(function(response) {
-  return response.json ()
-})
-// the second .then calls the function.
-.then(function(json) {
-   console.log(json);
-makeCards(json);
-})
+	// the first .then convert the result to JSON format.
+	.then(function(response) {
+		return response.json ()
+	})
+	// the second .then calls the function.
+	.then(function(json) {
+		console.log(json);
+		makeCards(json);
+	})
--- course_assignment/scripts/specific.js
+++ course_assignment/scripts/specific.js
@@ -1,5 +1,15 @@
 // refer to question 2 before development starts for scope document
 // get URL query string
+fetch('https://api.magicthegathering.io/v1/cards/3b072bac-4508-5bfa-adc3-13a9163284d3')
+  .then(function(response) {
+  return response.json ()
+})
+   // the second .then calls the function.
+  .then(function(json) {
+   console.log(json);
+   getQueryStringValue("id");
+})
+
 function getQueryStringValue (key) {
     return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 }

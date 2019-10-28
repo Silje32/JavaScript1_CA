68df11796a34df0f5631ae31f6a31fd1ff48f282
siljeheldalmonsen <silje24@yahoo.com>
2019-10-28 22:04:11 +0100

    rewrote contact.js

--- course_assignment/scripts/contact.js
+++ course_assignment/scripts/contact.js
@@ -1,39 +1,64 @@
 // refer to question 4 before development starts for scope document
 
 //Validating that "name" and "last name" inputs are not empty.
-function validateWholeName(name) {
-  var namePattern = /(\w+)/s(\w+)/;
-  if (name.match(namePattern)) {
-     result.innerHTML = 'This field cannot be blank';
-  }  else {
-     result.innerHTML = 'The last name is <strong>valid!</strong>';
+function validateFirstName(name) {
+   var firstNameError = document.querySelector("#firstNameError");
+
+   if (name.length === 0) {
+       firstNameError.style.display = "block";
+   }
+   else {
+       firstNameError.style.display = "none";
+   }
+ }
+
+ function validateLastName(lastname) {
+    var lastNameError = document.querySelector("#lastNameError");
+
+    if (name.length === 0) {
+        lastNameError.style.display = "block";
     }
-}
-
-//Validating that the phone number is correct.
-function validatePhone(phone) {
-  var phonePattern = /^\d{3}\ \d{3}\ \d{4}\$/;
-  if (phone.match(phonePattern)) {
-     result.innerHTML = 'Please enter a correct phone number' ;
-  }  else {
-     result.innerHTML = 'The phone number is <strong>valid!</strong>';
+    else {
+        lastNameError.style.display = "none";
+    }
   }
-}
 
-//Validating that the e-mail adress are correct.
-function validateEmail(email) {
-  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  if (email.match(emailPattern)) {
-     result.innerHTML = 'Please enter a correct e-mail address';
-  }  else {
-     result.innerHTML = 'The e-mail adress is <strong>valid!</strong>';
-  }
-}
 
-//the function will be called inside the form's eventListener
-  var form = document.querySelector('form');
-  form.addEventListener('submit', function() {
-      validateWholeName(this.name.value);
-      validatePhone(this.phone.value);
-      validateEmail(this.email.value);
-});
+ //Validating that the phone number is correct.
+ function validatePhone(phone) {
+   var phoneError = document.querySelector("#phoneError");
+
+   if (phone === /^\d{3}\ \d{3}\ \d{4}\$/); {
+       phoneError.style.display = "block";
+   }
+   else {
+       phoneError.style.display = "none";
+   }
+ }
+
+
+ //Validating that the e-mail adress is correct.
+ function validateEmail(email) {
+   var emailError = document.querySelector("#emailError");
+
+   if (email ===  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/); {
+     emailError.style.display = "block";
+   }
+   else {
+     emailError.style.display = "none";
+   }
+ }
+
+
+ //the function will be called inside the form's eventListener
+ var form = document.querySelector('form');
+
+ form.addEventListener('submit', function(event) {
+
+   event.preventDefault();
+
+   validateFirstName(this.firstName.value);
+   validateLastName(this.lastName.value);
+   validatePhone(this.phone.value);
+   validateEmail(this.email.value);
+ });

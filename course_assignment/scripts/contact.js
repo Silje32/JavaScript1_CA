5b2d82ba157c321c02379f856612ea3bb36e78c9
siljeheldalmonsen <silje24@yahoo.com>
2019-10-29 11:40:49 +0100

    contact page done

--- course_assignment/scripts/contact.js
+++ course_assignment/scripts/contact.js
@@ -2,51 +2,53 @@
 
 //Validating that "name" and "last name" inputs are not empty.
 function validateFirstName(name) {
-   var firstNameError = document.querySelector("#firstNameError");
+	 var firstNameError = document.querySelector("#firstNameError");
 
-   if (name.length === 0) {
-       firstNameError.style.display = "block";
-   }
-   else {
-       firstNameError.style.display = "none";
-   }
+	 if (name.length === 0) {
+			 firstNameError.style.display = "block";
+	 }
+	 else {
+			 firstNameError.style.display = "none";
+	 }
  }
 
- function validateLastName(lastname) {
-    var lastNameError = document.querySelector("#lastNameError");
+function validateLastName(lastname) {
+	var lastNameError = document.querySelector("#lastNameError");
 
-    if (name.length === 0) {
-        lastNameError.style.display = "block";
-    }
-    else {
-        lastNameError.style.display = "none";
-    }
-  }
+	if (lastname.length === 0) {
+		lastNameError.style.display = "block";
+	}
+	else {
+		lastNameError.style.display = "none";
+	}
+}
 
 
  //Validating that the phone number is correct.
  function validatePhone(phone) {
-   var phoneError = document.querySelector("#phoneError");
+	var phoneError = document.querySelector("#phoneError");
+	var phonePattern = /^\d{3}\ \d{3}\ \d{4}$/;
 
-   if (phone === /^\d{3}\ \d{3}\ \d{4}\$/); {
-       phoneError.style.display = "block";
-   }
-   else {
-       phoneError.style.display = "none";
-   }
- }
+	if (!phone.match(phonePattern)) {
+		phoneError.style.display = "block";
+	}
+	else {
+		phoneError.style.display = "none";
+	}
+}
 
 
  //Validating that the e-mail adress is correct.
  function validateEmail(email) {
-   var emailError = document.querySelector("#emailError");
+	 var emailError = document.querySelector("#emailError");
+   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-   if (email ===  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/); {
-     emailError.style.display = "block";
-   }
-   else {
-     emailError.style.display = "none";
-   }
+	 if (!email.match(emailPattern)) {
+		 emailError.style.display = "block";
+	 }
+	 else {
+		 emailError.style.display = "none";
+	 }
  }
 
 
@@ -55,10 +57,10 @@ function validateFirstName(name) {
 
  form.addEventListener('submit', function(event) {
 
-   event.preventDefault();
+	 event.preventDefault();
 
-   validateFirstName(this.firstName.value);
-   validateLastName(this.lastName.value);
-   validatePhone(this.phone.value);
-   validateEmail(this.email.value);
+	 validateFirstName(this.firstName.value);
+	 validateLastName(this.lastName.value);
+	 validatePhone(this.phone.value);
+	 validateEmail(this.email.value);
  });
